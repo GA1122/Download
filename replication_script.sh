7f97711f9401f94e0ad75dbec4f6e4f6fecfcac7
GA1122 <97390756+GA1122@users.noreply.github.com>
2024-04-30 12:45:59 +0200

    Update paths

--- replication_script.sh
+++ replication_script.sh
@@ -42,4 +42,4 @@ mkdir o`echo $$`
 cd o`echo $$`
 
 # Run the actual experiment.
-python /home/gas690/DeepDFA/DDFA/code_gnn/main_cli.py fit --config /home/gas690/DeepDFA/DDFA/configs/config_bigvul.yaml --config /home/gas690/DeepDFA/DDFA/configs/config_ggnn.yaml $@
+python code_gnn/main_cli.py fit --config configs/config_bigvul.yaml --config configs/config_ggnn.yaml $@
