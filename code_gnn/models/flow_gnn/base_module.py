4246600a420a34eb2260737fe5b7703a37da0cdc
GA1122 <97390756+GA1122@users.noreply.github.com>
2024-06-13 17:32:01 +0200

    Update base_module.py

--- code_gnn/models/flow_gnn/base_module.py
+++ code_gnn/models/flow_gnn/base_module.py
@@ -356,7 +356,7 @@ class BaseModule(pl.LightningModule):
         preds, labels = self.test_preds.compute(), self.test_labels.compute().int()
 
         precision, recall, thresholds = self.test_pr_curve(preds, labels)
-        pd.DataFrame({"precision": precision.tolist(), "recall": recall.tolist(), "thresholds": thresholds.tolist() + [1]}).to_csv("pr.csv")
+        pd.DataFrame({"precision": precision.tolist(), "recall": recall.tolist(), "thresholds": thresholds.tolist()}).to_csv("pr.csv")
         #precision_bin, recall_bin, thresholds_bin = self.test_pr_curve_bin(preds, labels)
         #pd.DataFrame({"precision": precision_bin.tolist(), "recall": recall_bin.tolist(), "thresholds": thresholds_bin.tolist() + [1]}).to_csv("pr_binned.csv")
 
