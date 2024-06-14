776aa157b8d6cc3ddca37df6ea5a111f48016ca5
GA1122 <97390756+GA1122@users.noreply.github.com>
2024-06-14 14:03:38 +0200

    Update datasets.py

--- code_gnn/sastvd/scripts/sastvd/helpers/datasets.py
+++ code_gnn/sastvd/scripts/sastvd/helpers/datasets.py
@@ -386,7 +386,7 @@ def ds_filter(
     # Filter out samples with no lineNumber from Joern output
     if check_valid:
         valid_cache = svd.cache_dir() / f"{dsname}_valid_{sample_mode}.csv"
-        if valid_cache.exists():
+        if False:
             valid_cache_df = pd.read_csv(valid_cache, index_col=0)
         else:
             valid = svd.dfmp(
