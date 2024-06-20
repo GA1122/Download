3652234e9b588804fafffb7658448fa82e75d3ba
GA1122 <97390756+GA1122@users.noreply.github.com>
2024-06-20 14:00:11 +0200

    Update base_module.py

--- code_gnn/models/base_module.py
+++ code_gnn/models/base_module.py
@@ -170,6 +170,7 @@ class BaseModule(pl.LightningModule):
 
     def training_step(self, batch_data, batch_idx):
         batch, extrafeats = batch_data
+        print(batch)
         label = self.get_label(batch)
         out = self.forward(batch, extrafeats)
         if self.hparams.label_style == "dataflow_solution_in":
@@ -210,6 +211,7 @@ class BaseModule(pl.LightningModule):
 
     def validation_step(self, batch_data, batch_idx, dataloader_idx=0):
         batch, extrafeats = batch_data
+        print(batch)
         label = self.get_label(batch)
         out = self.forward(batch, extrafeats)
         if self.hparams.label_style == "dataflow_solution_in":
@@ -248,6 +250,7 @@ class BaseModule(pl.LightningModule):
             end = torch.cuda.Event(enable_timing=True)
 
         batch, extrafeats = batch_data
+        print(batch)
         label = self.get_label(batch)
         if do_time:
             start.record()
