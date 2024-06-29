12c7043d03d264d99f698f307fab29cea15ab536
GA1122 <97390756+GA1122@users.noreply.github.com>
2024-06-29 17:21:09 +0200

    Update base_module.py

--- code_gnn/models/flow_gnn/base_module.py
+++ code_gnn/models/flow_gnn/base_module.py
@@ -91,7 +91,7 @@ class BaseModule(pl.LightningModule):
             print("Number of graphs - " + str(len(graphs)) + "\n")
             print("Label content:\n")
             for g in graphs:
-                print("Graph content : number of nodes - " + str(g.number_of_nodes()) + ", number of edges - " + str(g.num_edges()) + ", classification length - " + str(len(g.ndata["_VULN"])) + ", classification 0 content - " + str(torch.sum(g.ndata["_VULN"] == 0)) + "\n")
+                print("Graph info: '_ABS_DATAFLOW' - " + str(g.ndata['_ABS_DATAFLOW']) + ", '_ABS_DATAFLOW_api' - " + str(g.ndata['_ABS_DATAFLOW_api']) + ", '_ABS_DATAFLOW_datatype' - " + str(g.ndata["_ABS_DATAFLOW_datatype"]) + ", '_ABS_DATAFLOW_literal' - " + str(g.ndata["_ABS_DATAFLOW_literal"]) + ", '_ABS_DATAFLOW_operator' - " + str(g.ndata["_ABS_DATAFLOW_operator"]) + "\n")
             print("\n")
             print("Label - " + str(label) + "\n")
             print("\n")
