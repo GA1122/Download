fa1b4243545c8854e46e20a852c5e27d708d464b
GA1122 <97390756+GA1122@users.noreply.github.com>
2024-06-30 16:00:35 +0200

    Update base_module.py

--- code_gnn/models/flow_gnn/base_module.py
+++ code_gnn/models/flow_gnn/base_module.py
@@ -91,7 +91,7 @@ class BaseModule(pl.LightningModule):
             label = []
             for g in graphs:
                 if (g.number_of_nodes() in databaseDF["num_nodes"].values & g.num_edges() in databaseDF["num_edges"].values):
-                    if (databaseDF.loc[(databaseDF["num_nodes"] == g.number_of_nodes())]["ID"] == databaseDF.loc[(databaseDF["num_edges"] == g.num_edges())]["ID"]):
+                    if (databaseDF.loc[(databaseDF["num_nodes"] == g.number_of_nodes())]["ID"].iloc[0] == databaseDF.loc[(databaseDF["num_edges"] == g.num_edges())]["ID"].iloc[0]):
                         vuln = databaseDF.loc[(databaseDF["num_nodes"] == g.number_of_nodes()) & (databaseDF["num_edges"] == g.num_edges())]["vuln"].values
                 else:
                     vuln = 0
