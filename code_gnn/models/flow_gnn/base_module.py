75ece98d29d0aa26b44db9abc4db139d5484a3a7
GA1122 <97390756+GA1122@users.noreply.github.com>
2024-06-30 19:21:41 +0200

    Update base_module.py

--- code_gnn/models/flow_gnn/base_module.py
+++ code_gnn/models/flow_gnn/base_module.py
@@ -93,6 +93,9 @@ class BaseModule(pl.LightningModule):
             for g in graphs:
                 if (g.number_of_nodes() in databaseDF["num_nodes"].values & g.num_edges() in databaseDF["num_edges"].values):
                     if (databaseDF.loc[(databaseDF["num_nodes"] == g.number_of_nodes())]["ID"].first_valid_index() == databaseDF.loc[(databaseDF["num_edges"] == g.num_edges())]["ID"].first_valid_index()):
+                        print("\n")
+                        print("Muudan 1ks")
+                        print("\n")
                         vuln = databaseDF.loc[(databaseDF["num_nodes"] == g.number_of_nodes()) & (databaseDF["num_edges"] == g.num_edges())]["vuln"].values
                     else:
                         vuln = 0
