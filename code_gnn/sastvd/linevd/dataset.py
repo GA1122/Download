f6c92d62d34819f200107f1fe7f2f7f22b76a2e1
GA1122 <97390756+GA1122@users.noreply.github.com>
2024-07-06 17:18:57 +0200

    Update dataset.py

--- code_gnn/sastvd/linevd/dataset.py
+++ code_gnn/sastvd/linevd/dataset.py
@@ -46,7 +46,7 @@ class BigVulDatasetLineVD(svddc.BigVulDataset):
 
     def item(self, _id):
         """Get item by id."""
-        return self.graphs_by_id[_id]
+        return self.graphs_by_id[_id], self.extrafeats_by_id[_id]
 
     def __getitem__(self, idx):
         """Override getitem."""
